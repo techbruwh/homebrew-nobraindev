2c5e938e04c5ab92f4d33437c62c741e1c6d6521
Faisal Morensya <lloistborn@live.com>
2025-10-11 14:35:32 +0900

    improve cask formula

--- Casks/nobraindev.rb
+++ Casks/nobraindev.rb
@@ -2,14 +2,12 @@ cask "nobraindev" do
   version "0.1.0"
 
   on_arm do
-    url "https://github.com/techbruwh/nobraindev/releases/download/v0.1.0/NoBrainDev_0.1.0_aarch64.dmg",
-        verified: "github.com/techbruwh/nobraindev/"
+    url "https://github.com/techbruwh/nobraindev/releases/download/v#{version}/NoBrainDev_#{version}_aarch64.dmg"
     sha256 "9e2476ac2bcca850621984a494336b2569749e104f0d639b785462ff762cf571"
   end
 
   on_intel do
-    url "https://github.com/techbruwh/nobraindev/releases/download/v0.1.0/NoBrainDev_0.1.0_x64.dmg",
-        verified: "github.com/techbruwh/nobraindev/"
+    url "https://github.com/techbruwh/nobraindev/releases/download/v#{version}/NoBrainDev_#{version}_x64.dmg"
     sha256 "674307eae2572332a2edde06011125f51b8763ed5fddfa16a0e1a43946c2cb79"
   end
   name "NoBrainDev"
